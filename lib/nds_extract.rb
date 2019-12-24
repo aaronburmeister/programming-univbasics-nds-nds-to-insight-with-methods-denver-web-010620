b2b376a29a156aa71987444dc873251e71f091e9
Aaron Burmeister <f34r91@gmail.com>
2019-12-24 21:41:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,6 +8,7 @@ def gross_for_director(director_data)
   movie = 0
   while movie < director_data[:movies].length do
     gross += director_data[:movies][movie][:worldwide_grosses]
+    movie += 1
   end
   return {director_data[:name] => gross }
 end
